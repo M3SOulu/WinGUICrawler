351820cc8f6245a5d68075eb231cecfeeef395b9
marukosan <watashiwamarukosandesu@gmail.com>
2021-07-29 09:19:33 +0300

    Adding new folder

--- fmm-lung-nodule-segmentation/Evaluation (Subjective)/Questionnaire/extractform.py
+++
@@ -1,7 +0,0 @@
-import PyPDF2 as pypdf
-import sys
-
-INPUT_FILE = sys.argv[1]
-pdfobject=open(INPUT_FILE,'rb')
-pdf=pypdf.PdfFileReader(pdfobject)
-print(pdf.getFormTextFields())
--- fmm-lung-nodule-segmentation/README.md
+++
@@ -1,1 +0,0 @@
-# fmm-lung-nodule-segmentation
